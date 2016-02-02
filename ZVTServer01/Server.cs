0cc619ace9ce84aa62f40ea6c46f0ea33048931b
ro bra <rolbra@outlook.de>
2016-02-02 17:06:49 +0100

    erster Dateiupload

---
+++ ZVTServer01/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ZVTServer01
+{
+    public class Server
+    {
+        //Konstruktoren
+        public Server() : this("", 8000) { }
+
+        public Server(string host, int port)
+        {
+            SocServerListen.Bind()
+        }
+
+        public long HostAdr = 
+        System.Net.IPEndPoint IpEp = new System.Net.IPEndPoint();
+        Socket SocServerListen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+    }
+}
